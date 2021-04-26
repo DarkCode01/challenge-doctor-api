2f285d74665b1adfb830a41baa4dee63714daf81
darkcode01 <j0s3s3gur4@gmail.com>
2021-04-26 15:21:13 -0400

    created models and config files for sequelize

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-app.use(logger('dev'));
+const app = express();
+
+app.use(logger());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
